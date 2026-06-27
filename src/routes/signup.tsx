e39de13ef9e365435bb6223c296b8ad44549665b
imbagila <roid.rabbani@gmail.com>
2026-06-28 02:40:06 +0700

    run: npx shadcn@latest add signup-04

---
+++ src/routes/signup.tsx
@@ -0,0 +1,34 @@
+import Galaxy from "#/components/Galaxy";
+import { SignupForm } from "#/components/signup-form";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/signup")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="relative flex min-h-svh flex-col items-center justify-center overflow-hidden p-6 md:p-10">
+      <div className="absolute inset-0">
+        <Galaxy
+          transparent={false}
+          mouseRepulsion={false}
+          mouseInteraction={false}
+          density={1}
+          glowIntensity={0.3}
+          saturation={0}
+          hueShift={140}
+          twinkleIntensity={0.3}
+          rotationSpeed={0.1}
+          repulsionStrength={2}
+          autoCenterRepulsion={0}
+          starSpeed={0.5}
+          speed={1}
+        />
+      </div>
+      <div className="relative z-10 w-full max-w-sm md:max-w-4xl">
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
