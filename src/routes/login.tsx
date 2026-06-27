552536f6b437cb165be5b6f5526a8818bd2df1c2
imbagila <roid.rabbani@gmail.com>
2026-06-28 02:34:35 +0700

    run: bun x --bun shadcn@latest add @react-bits/Galaxy-TS-TW

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,4 @@
+import Galaxy from "#/components/Galaxy";
 import { LoginForm } from "#/components/login-form";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -7,8 +8,25 @@ export const Route = createFileRoute("/login")({
 
 function RouteComponent() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
+    <div className="relative flex min-h-svh flex-col items-center justify-center overflow-hidden p-6 md:p-10">
+      <div className="absolute inset-0">
+        <Galaxy
+          transparent={false}
+          mouseRepulsion={false}
+          mouseInteraction={false}
+          density={1}
+          glowIntensity={0.3}
+          saturation={0}
+          hueShift={140}
+          twinkleIntensity={0.3}
+          rotationSpeed={0.1}
+          repulsionStrength={2}
+          autoCenterRepulsion={0}
+          starSpeed={0.5}
+          speed={1}
+        />
+      </div>
+      <div className="relative z-10 w-full max-w-sm md:max-w-4xl">
         <LoginForm />
       </div>
     </div>
